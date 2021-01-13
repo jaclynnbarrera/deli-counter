96452a03b302f19bf5e9d0ba377f85b72008601d
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-13 18:16:35 -0500

    commit tree 13a79c5534aae4af9ff2daaeb7f686ffee29088a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def line(arr)
+    if arr.empty?
+        puts "The line is currently empty."
+    else
+        current_line = "The line is currently:"
+        arr.each_with_index do |e,i| 
+            current_line << " #{i+1}. #{e}"
+        end 
+        puts current_line
+    end 
+end
+
+def take_a_number(arr, name)
+    arr << name
+    puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
+
+def now_serving(arr)
+    if arr.empty?
+        puts "There is nobody waiting to be served!"
+    else
+    puts "Currently serving #{arr[0]}."
+    arr.shift
+    end 
+end
